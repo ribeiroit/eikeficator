c4e1d262e1234928bc7d63e0770738b86d108dcf
Hedcler Morais <hedclermorais@gmail.com>
2013-08-28 17:59:03 -0300

    Create gem.js

---
+++ gem.js
@@ -0,0 +1,5 @@
+var string = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","U","V","W","X","Y","Z"]
+var e = Math.floor((Math.random()*25)+1);
+var b = Math.floor((Math.random()*25)+1);
+var corp = string[e]+string[b]+'X';
+alert("Eike's next corp name is " + corp + " eike_corp");
